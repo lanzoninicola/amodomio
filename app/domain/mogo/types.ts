faba270d939ebb40c17f910e12c09e493d7203f3
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2024-01-02 15:15:01 -0300

    feat: working on kanban time

---
+++ app/domain/mogo/types.ts
@@ -0,0 +1,54 @@
+export interface MogoBaseOrder {
+  Id: number;
+  NumeroPedido: string;
+  Itens: {
+    IdProduto: number;
+    IdItemPedido: number;
+    Descricao: string;
+    Observacao: string;
+    Quantidade: number;
+    ValorUnitario: number;
+    Adicionais: any[]; // Pode ser um array de objetos com propriedades específicas
+    Sabores: {
+      Descricao: string;
+      Quantidade: number;
+      Valor: number;
+    }[];
+  }[];
+  SubTotal: number;
+  TaxaEntrega: number;
+  DataPedido: string;
+  HoraPedido: string;
+  HoraEntrega: string;
+  HoraAcerto: string;
+  FormaPagamento: string;
+  TrocoDelivery: number;
+  Logradouro: string;
+  Numero: string;
+  Bairro: string;
+  Cidade: string;
+  UF: string;
+  ReaisDesconto: number;
+  PercentualDesconto: number;
+  StatusPedido: number;
+  StatusEntrega: number;
+  HoraEntregaTxt: string;
+  DataEntregaTxt: string | null;
+  CodObsEntrega: number;
+  DataSaidaEntregador: string;
+  Adiantamentos: any; // Pode ser de um tipo específico ou null
+  IdPedRemoteDevice: string;
+  ObsEntrega: any; // Pode ser de um tipo específico ou null
+  Vendedor: any; // Pode ser de um tipo específico ou null
+  Cliente: string;
+  IdCliente: number;
+  IdEntregador: number;
+  Latitude: string;
+  Longitude: string;
+  ReferenceIFood: any; // Pode ser de um tipo específico ou null
+  PickupCode: any; // Pode ser de um tipo específico ou null
+}
+
+export interface MogoOrderWithDiffTime extends MogoBaseOrder {
+  diffMinutesToNow: number;
+}
