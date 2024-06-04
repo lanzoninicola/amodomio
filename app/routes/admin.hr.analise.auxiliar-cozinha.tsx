95de66f61c7b1232f7260bbb7dd46b31fdff6b90
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2024-06-04 09:03:53 -0300

    feat: update hr pages

--- app/routes/admin.hr.analise.auxiliar-cozinha.tsx
+++ app/routes/admin.hr.analise.auxiliar-cozinha.tsx
@@ -1,12 +1,15 @@
 import Container from "~/components/layout/container/container"
 import candidatosJson from "~/domain/hr/db/vagas-24-05-25-candidatos.json"
 import right from "~/utils/right"
- import { ok } from "~/utils/http-response.server"
+import { ok } from "~/utils/http-response.server"
 import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert"
 import { useLoaderData } from "@remix-run/react"
 import { AlertCircle } from "lucide-react"
 import { cn } from "~/lib/utils"
 import { Separator } from "~/components/ui/separator"
+import React from "react"
+import Badge from "~/components/primitives/badge/badge"
+import { V2_MetaFunction } from "@remix-run/node"
 
 
 
@@ -22,9 +25,6 @@ export async function loader() {
         }
     })
 
-
-    console.log({ candidatos })
-
     return ok({ candidatos })
 }
 
@@ -32,8 +32,6 @@ export default function VagaAuxiliarCozinha() {
 
     const loaderData = useLoaderData<typeof loader>()
     const candidatos = loaderData?.payload?.candidatos || []
-
-
 
 
     if (loaderData?.status !== 200) {
@@ -56,26 +54,45 @@ export default function VagaAuxiliarCozinha() {
                         <li key={c.id} className={
                             cn(
                                 "flex flex-col gap-2 border rounded-lg p-4",
-                                c.Recomendacao <= 5 && "bg-red-300",
-                                c.Recomendacao > 5 && c.Recomendacao <= 7 && "bg-yellow-300",
-                                c.Recomendacao > 7 && "bg-green-300"
                             )
                         }>
-                            <span><strong>Nome: </strong>{c.Nome}</span>
-                            <span><strong>Idade: </strong>{c.Idade}</span>
-                            <span><strong>Sexo: </strong>{c.Sexo}</span>
-                            <span><strong>Telefone: </strong>{c.Telefone}</span>
-                            <span><strong>Experienca: </strong>{c.Experienca}</span>
-                            <span><strong>Disponibilidade: </strong>{c.Disponibilidade}</span>
-                            <span><strong>Motivação: </strong>{c.Motivacao}</span>
-                            <Separator color="black" />
-                            <span><strong>Comprometimento e Colaboração: </strong>{c.ComprometimentoColaboracao}</span>
-                            <span><strong>Opinião ChatGPT: </strong>{c.Opiniao}</span>
-                            <span><strong>Recomendação: </strong>{c.Recomendacao}</span>
+                            <div className="flex justify-between items-center flex-reverse">
+                                <Row title="Nome">{c.Nome}</Row>
+                                <Badge className={
+                                    cn(
+                                        c.Recomendacao <= 5 && "bg-red-300",
+                                        c.Recomendacao > 5 && c.Recomendacao <= 7 && "bg-yellow-300",
+                                        c.Recomendacao > 7 && "bg-green-300"
+                                    )
+                                }>
+                                    {c.Recomendacao <= 5 ? "Baixa" : c.Recomendacao > 5 && c.Recomendacao <= 7 ? "Media" : "Alta"}
+                                </Badge>
+                            </div>
+                            <Row title="Idade">{c.Idade}</Row>
+                            <Row title="Sexo">{c.Sexo}</Row>
+                            <Row title="Telefone">{c.Telefone}</Row>
+                            <Row title="Experienca">{c.Experienca}</Row>
+                            <Row title="Disponibilidade">{c.Disponibilidade}</Row>
+                            <Row title="Motivação">{c.Motivacao}</Row>
+                            <Separator color="black" className="my-2" />
+                            <Row title="Comprometimento e Colaboração">{c.ComprometimentoColaboracao}</Row>
+                            <Row title="Opinião ChatGPT">{c.Opiniao}</Row>
+                            <Row title="Recomendação">{c.Recomendacao}</Row>
                         </li>
                     ))
                 }
             </ul>
         </Container>
+    )
+}
+
+interface RowProps {
+    title: string
+    children: React.ReactNode
+}
+
+function Row({ title, children }: RowProps) {
+    return (
+        <p><span className="font-semibold">{title}: </span>{children}</p>
     )
 }
