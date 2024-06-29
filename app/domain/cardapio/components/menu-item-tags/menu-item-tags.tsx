f8e0ece8ce067ed899f58f0b122b26f75bbf6fcc
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2024-06-29 15:46:48 -0300

    feat: some code

--- app/domain/cardapio/components/menu-item-tags/menu-item-tags.tsx
+++ app/domain/cardapio/components/menu-item-tags/menu-item-tags.tsx
@@ -4,12 +4,26 @@ import MenuItemTagSelector from "./menu-item-tag-selector/menu-item-tag-selector
 import { MenuItemTag } from "@prisma/client"
 import { Form, useFetcher, useSubmit } from "@remix-run/react"
 import { jsonStringify } from "~/utils/json-helper"
+import { useState } from "react"
 
 
 interface MenuItemTagsProps {
     item: MenuItemWithAssociations
 }
 export default function MenuItemTags({ item }: MenuItemTagsProps) {
+
+    const [currentTags, setCurrentTags] = useState(item?.tags || [])
+
+
+    function removeTag(itemId: string, name: string) {
+        const nextTags = currentTags.slice(currentTags.findIndex(t => t.name === name && t.menuItemId === itemId), 1)
+        setCurrentTags(nextTags)
+    }
+
+    function addTag(tag: MenuItemTag) {
+        setCurrentTags([...currentTags, tag])
+    }
+
     return (
         <>
             <span className="font-semibold text-sm col-span-1">Tags</span>
@@ -17,20 +31,23 @@ export default function MenuItemTags({ item }: MenuItemTagsProps) {
 
                 <MenuItemTagSelector item={item} className="col-span-3" />
                 <div className="flex gap-2 h-fit mt-2">
-                    {item?.tags.filter(t => t.deletedAt === null).map(t =>
-                        <BadgeTag key={t.id} item={item} tag={t} />)}
+                    {currentTags.filter(t => t.deletedAt === null).map(t =>
+                        <BadgeTag key={t.id} item={item} tag={t} removeTag={removeTag} />)}
                 </div>
             </div>
         </>
     )
 }
 
-function BadgeTag({ item, tag }: { item: MenuItemWithAssociations, tag: MenuItemTag }) {
+interface BadgeTagProps {
+    item: MenuItemWithAssociations
+    tag: MenuItemTag
+    removeTag: (itemId: string, name: string) => void
+}
 
-    const submit = useSubmit()
+function BadgeTag({ item, tag, removeTag }: BadgeTagProps) {
 
     const fetcher = useFetcher()
-
 
     return (
         <Badge>
@@ -40,6 +57,9 @@ function BadgeTag({ item, tag }: { item: MenuItemWithAssociations, tag: MenuItem
                 value="menu-item-tag-remove"
                 className="hover:underline"
                 onClick={() => {
+
+                    removeTag(item.id, tag.name)
+
                     fetcher.submit({
                         tagName: tag.name,
                         itemId: item.id,
