c6c5c98074e78c196ba766ae5194c676a1df5b1a
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2024-07-20 10:44:21 -0300

    feat: extending prisma client class to get some info from the datasource

--- app/lib/prisma/client.server.ts
+++ app/lib/prisma/client.server.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const env = process.env.NODE_ENV || "production";
 const prismaDatasourceUrl = process.env?.PRISMA_DB_URL;
@@ -7,6 +7,30 @@ const prismaDevDatasourceUrl = process.env?.PRISMA_DB_DEV_URL;
 const datasourceUrl =
   env === "development" ? prismaDevDatasourceUrl : prismaDatasourceUrl;
 
-const prismaClient = new PrismaClient({ datasourceUrl });
+// const prismaClient = new PrismaClient({ datasourceUrl });
+
+class AMMPrismaClient<
+  T extends Prisma.PrismaClientOptions = Prisma.PrismaClientOptions
+> extends PrismaClient {
+  datasourceUrl: string | undefined;
+
+  dbName: string | undefined;
+
+  constructor(optionsArg?: Prisma.Subset<T, Prisma.PrismaClientOptions>) {
+    super(optionsArg);
+
+    this.datasourceUrl = datasourceUrl;
+
+    if (datasourceUrl) {
+      const match = datasourceUrl.match(/\/([^\/?]+)(?:\?|$)/);
+
+      if (match) {
+        this.dbName = match[1];
+      }
+    }
+  }
+}
+
+const prismaClient = new AMMPrismaClient({ datasourceUrl });
 
 export default prismaClient;
