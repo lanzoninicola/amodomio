0225f6a20defef274f30f3d67ecbd5cdbb1bae4e
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2024-07-28 15:45:06 -0300

    feat: workin on cardapio

--- app/routes/cardapio-web.tsx
+++ app/routes/cardapio-web.tsx
@@ -1,46 +1,83 @@
-import { Link, Outlet } from "@remix-run/react";
-import { ArrowRight } from "lucide-react";
-import Container from "~/components/layout/container/container";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { LoaderArgs, V2_MetaFunction } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { ArrowRight, SearchIcon, XIcon } from "lucide-react";
+import { useState } from "react";
 import ExternalLink from "~/components/primitives/external-link/external-link";
 import Logo from "~/components/primitives/logo/logo";
-import WhatsappExternalLink from "~/components/primitives/whatsapp/whatsapp-external-link";
-import WhatsAppIcon from "~/components/primitives/whatsapp/whatsapp-icon";
-import { Separator } from "~/components/ui/separator";
+import { Input } from "~/components/ui/input";
+import { menuItemTagPrismaEntity } from "~/domain/cardapio/menu-item-tags.prisma.entity.server";
+import { MenuItemWithAssociations, menuItemPrismaEntity } from "~/domain/cardapio/menu-item.prisma.entity.server";
+import { prismaIt } from "~/lib/prisma/prisma-it.server";
+import { badRequest, ok } from "~/utils/http-response.server";
 
+export interface CardapioOutletContext {
+    items: MenuItemWithAssociations[]
+}
 
+export const meta: V2_MetaFunction = () => {
+    return [
+        {
+            name: "title",
+            content: "Cardápio Pizzaria A Modo Mio - Pato Branco",
+        }
+    ];
+};
+
+export async function loader({ request }: LoaderArgs) {
+    // @ts-ignore
+    const [errItems, items] = await prismaIt(menuItemPrismaEntity.findAll({
+        where: {
+            visible: true
+        },
+        option: {
+            sorted: true,
+            direction: "asc"
+        },
+        mock: true
+    }))
+
+    if (errItems) {
+        return badRequest(errItems)
+    }
+
+    const [_, tags] = await prismaIt(menuItemTagPrismaEntity.findAllDistinct())
+
+    return ok({ items, tags })
+
+}
 
 
 export default function CardapioWeb() {
+    const loaderData = useLoaderData<typeof loader>()
+    const items = loaderData?.payload.items as MenuItemWithAssociations[] || []
+
     return (
         <>
-            <CardapioHeader />
+            <CardapioHeader items={items} />
             {/* <Featured /> */}
-            <Outlet />
+            <Outlet context={{ items }} />
             <CardapioFooter />
         </>
     )
 }
 
-function CardapioHeader() {
+function CardapioHeader({ items }: { items: MenuItemWithAssociations[] }) {
+    const [showSearch, setShowSearch] = useState(false)
+
     return (
-        <header className="bg-white shadow fixed top-0 w-screen border-b-slate-100 px-4 py-3">
-            <div className="flex justify-between items-center  ">
-                <div className="flex gap-2 items-center">
-                    <Link to="/cardapio-web">
+        <header className="bg-white shadow fixed top-0 w-screen border-b-slate-100 px-4 py-3 z-50" >
+            <div className="flex flex-col">
+                <div className="grid grid-cols-3 items-center w-full">
+                    <HamburgerMenuIcon className="w-6 h-6" />
+                    <Link to="/cardapio-web" className="flex justify-center">
                         <Logo color="black" className="w-[60px]" tagline={false} />
                     </Link>
-                    <h1 className="font-semibold text-gray-900 font-body-website">Cardápio</h1>
+                    <div className="flex gap-2 justify-end" onClick={() => setShowSearch(!showSearch)}>
+                        <SearchIcon />
+                    </div>
                 </div>
-                <div className="flex gap-2 items-center">
-                    <WhatsappExternalLink phoneNumber="46991272525"
-                        ariaLabel="Envia uma mensagem com WhatsApp"
-                        message={"Olá, gostaria fazer um pedido"}
-                        className="flex flex-col gap-1 justify-center items-center"
-                    >
-                        <WhatsAppIcon color="black" />
-                        {/* <span className="text-[10px] tracking-wide  font-body-website">Atendimento</span> */}
-                    </WhatsappExternalLink>
-                </div>
+                {showSearch && <CardapioSearch items={items} setShowSearch={setShowSearch} />}
             </div>
         </header>
     )
@@ -61,4 +98,64 @@ function CardapioFooter() {
             </div>
         </footer>
     )
+}
+
+function CardapioSearch({ items, setShowSearch }: {
+    items: MenuItemWithAssociations[],
+    setShowSearch: React.Dispatch<React.SetStateAction<boolean>>
+}) {
+    const [currentItems, setCurrentItems] = useState<any[]>(items);
+    const [search, setSearch] = useState("")
+
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+        const value = event.target.value
+
+        setSearch(value)
+
+        if (!value) return setCurrentItems(items)
+
+        const searchedItems = items
+            .filter(item => item.name?.toLowerCase().includes(value.toLowerCase()) || item.ingredients?.toLowerCase().includes(value.toLowerCase()))
+
+        setCurrentItems(searchedItems)
+
+    }
+
+    return (
+        <div className="flex flex-col">
+            <div className="bg-white flex flex-col py-2">
+                <Input placeholder="Digitar 'abobrinha' ou 'vegetarianas'" className="font-body-website text-sm h-8" onChange={handleSearch} />
+                <ul className="flex flex-row gap-2 overflow-x-scroll pt-4">
+                    {currentItems.map((item) => (
+                        <li className="py-1 flex-1 min-w-[70px]" key={item.id}>
+                            <Link
+                                to={`/cardapio-web/#${item.id}`}
+                                className="flex flex-col gap-1 items-center w-full"
+                            >
+                                <div className="bg-center bg-cover bg-no-repeat w-8 h-8 rounded-lg "
+                                    style={{
+                                        backgroundImage: `url(${item.imageBase64 || "/images/cardapio-web-app/placeholder.png"})`,
+                                    }}></div>
+                                <span className="font-body-website text-[0.65rem] font-semibold leading-tight uppercase text-center">{item.name}</span>
+
+
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="flex justify-end  items-center px-2 gap-1"
+
+                onClick={() => setShowSearch(false)}
+            >
+                <XIcon className="w-[11px] h-[11px]" />
+                <p className="text-[9px] tracking-widest font-semibold uppercase" style={{
+                    lineHeight: "normal",
+                }}>Fechar</p>
+            </div>
+        </div>
+    )
+
+
 }
