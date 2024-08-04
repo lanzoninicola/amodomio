98edd7fcf441c6e627fa3be6f464732f9bbc6759
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2024-08-04 13:59:53 -0300

    feat: shareIt + meta

--- app/root.tsx
+++ app/root.tsx
@@ -108,7 +108,7 @@ const GTM_ID = process.env.GOOGLE_TAG_MANAGER_ID
 
 export default function App() {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="pt-br" className="scroll-smooth">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
