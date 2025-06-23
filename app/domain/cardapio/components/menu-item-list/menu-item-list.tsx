19f0861ca59d4496eda91885a6588981a51a27d5
Nicola Lanzoni <lanzoni.nicola@gmail.com>
2025-06-23 12:54:49 -0300

    working with group filter

--- app/domain/cardapio/components/menu-item-list/menu-item-list.tsx
+++ app/domain/cardapio/components/menu-item-list/menu-item-list.tsx
@@ -6,40 +6,23 @@ import { useFetcher } from "@remix-run/react"
 import { MenuItemWithAssociations } from "../../menu-item.prisma.entity.server"
 import { GripVertical } from "lucide-react"
 import { cn } from "~/lib/utils"
-import { Input } from "~/components/ui/input"
-import OptionTab from "~/components/layout/option-tab/option-tab"
 import { Separator } from "~/components/ui/separator"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select"
 import { MenuItemGroup } from "@prisma/client"
 import { Category } from "~/domain/category/category.model.server"
-import { group } from "console"
 
 interface MenuItemListProps {
-    initialItems: MenuItemWithAssociations[]
-    groups: MenuItemGroup[],
-    categories: Category[]
+    items: MenuItemWithAssociations[]
+    setItems: (items: MenuItemWithAssociations[]) => void
 }
 
 export type OveredPoint = "none" | "top" | "bottom"
 
-type MenuItemVisibilityFilterOption = "active" | "lancamento-futuro" | "venda-pausada" | "inactive"
 
-export default function MenuItemList({ initialItems, groups, categories }: MenuItemListProps) {
 
-    if (!initialItems || initialItems.length === 0) {
+export default function MenuItemList({ items, setItems }: MenuItemListProps) {
+
+    if (!items || items.length === 0) {
         return <NoRecordsFound text="Nenhum item encontrado" />
-    }
-
-    const [items, setItems] = useState<any[]>(initialItems.filter(i => i.visible === true && i.active === true));
-    const [search, setSearch] = useState("")
-
-    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value
-        setSearch(value)
-        if (!value) return setItems(initialItems)
-        const searchedItems = initialItems
-            .filter(item => item.name?.toLowerCase().includes(value.toLowerCase()) || item.ingredients?.toLowerCase().includes(value.toLowerCase()))
-        setItems(searchedItems)
     }
 
     const [dragEnable, setDragEnabled] = useState(false)
@@ -75,99 +58,17 @@ export default function MenuItemList({ initialItems, groups, categories }: MenuI
         );
     };
 
-    const [currentFilter, setCurrentFilter] = useState<MenuItemVisibilityFilterOption | null>("active")
-
-    // const [optVisibleItems, setOptVisibleItems] = useState<boolean | null>(true)
-    // const [optActiveItems, setOptActiveItems] = useState<boolean | null>(null)
-    // const [optUpcomingItems, setOptUpcomingItems] = useState<boolean | null>(null)
-
-    const handleVisibilityFilter = (option: MenuItemVisibilityFilterOption) => {
-
-        if (option === "active") {
-            setCurrentFilter("active")
-            setItems(initialItems.filter(item => item.active === true))
-        }
-
-        if (option === "inactive") {
-            setCurrentFilter("inactive")
-            setItems(initialItems.filter(item => item.active === false))
-        }
-
-        if (option === "lancamento-futuro") {
-            setCurrentFilter("lancamento-futuro")
-            setItems(initialItems.filter(item => item.active === true && item.upcoming === true))
-        }
-
-        if (option === "venda-pausada") {
-            setCurrentFilter("venda-pausada")
-            setItems(initialItems.filter(item => item.active === true && item.visible === false && item.upcoming === false))
-        }
-    }
-
-
 
     return (
         <div className="flex flex-col">
-            <div className="flex flex-col gap-4 md:grid md:grid-cols-8 md:gap-x-4 md:items-center">
-                <div className="md:col-span-2">
-                    <Select
-                        onValueChange={(value) => {
-                            handleVisibilityFilter(value as MenuItemVisibilityFilterOption)
-                        }}
-                        defaultValue={"active"}
-                    >
-                        <SelectTrigger className="w-full col-span-2">
-                            <SelectValue placeholder="Filtrar vendas" />
-                        </SelectTrigger>
-                        <SelectContent >
-                            <SelectItem value="active">Venda ativa</SelectItem>
-                            <SelectItem value="lancamento-futuro">Lançamento futuro</SelectItem>
-                            <SelectItem value="venda-pausada">Venda pausada</SelectItem>
-                            <SelectItem value="inactive">Inativos</SelectItem>
-                        </SelectContent>
-                    </Select>
-                </div>
-
-                <div className="md:col-span-2">
-                    <Select
-                        onValueChange={(value) => {
-                            handleVisibilityFilter(value as MenuItemVisibilityFilterOption)
-                        }}
-                        defaultValue={"active"}
-                    >
-                        <SelectTrigger className="w-full col-span-2">
-                            <SelectValue placeholder="Filtrar grupos" />
-                        </SelectTrigger>
-                        <SelectContent >
-                            {
-                                groups.map(g => {
-                                    return (
-                                        <SelectItem value={g.name}>{g.name}</SelectItem>
-                                    )
-                                })
-                            }
-                        </SelectContent>
-                    </Select>
-                </div>
 
 
-                <div className="items-center col-span-5 md:col-span-4" >
-                    <Input name="search" className="w-full" placeholder="Pesquisar..." onChange={(e) => handleSearch(e)} value={search} />
-                </div>
-
-                <div className="md:p-4 items-center col-span-3 md:col-span-2">
-                    <p className="text-sm cursor-pointer hover:underline text-muted-foreground leading-tight"
-                        onClick={() => setDragEnabled(!dragEnable)}
-                    >{dragEnable === true ? 'Desabilitar ordernamento' : 'Abilitar ordenamento'}</p>
-                </div>
-
+            <div className="md:p-4 items-center col-span-3 md:col-span-2">
+                <p className="text-sm cursor-pointer hover:underline text-muted-foreground leading-tight"
+                    onClick={() => setDragEnabled(!dragEnable)}
+                >{dragEnable === true ? 'Desabilitar ordernamento' : 'Abilitar ordenamento'}</p>
             </div>
-            {/* <div className="grid grid-cols-2 gap-4 md:flex   md:gap-4 items-center justify-center">
-                <OptionTab label="Venda ativa" onClickFn={() => handleVisibilityFilter("active")} highlightCondition={currentFilter === "active"} />
-                <OptionTab label="Lançamento futuro" onClickFn={() => handleVisibilityFilter("lancamento-futuro")} highlightCondition={currentFilter === "lancamento-futuro"} />
-                <OptionTab label="Venda pausada" onClickFn={() => handleVisibilityFilter("venda-pausada")} highlightCondition={currentFilter === "venda-pausada"} />
-                <OptionTab label="Inativos" onClickFn={() => handleVisibilityFilter("inactive")} highlightCondition={currentFilter === "inactive"} />
-            </div> */}
+
             <Separator className="my-4" />
             <ul className="flex flex-col gap-y-4">
                 {items.map((item, index) => (
--- app/routes/admin.gerenciamento.cardapio.main.list.tsx
+++ app/routes/admin.gerenciamento.cardapio.main.list.tsx
@@ -1,14 +1,17 @@
-import { MenuItem } from "@prisma/client";
+import { MenuItem, MenuItemGroup } from "@prisma/client";
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { Await, defer, useActionData, useLoaderData } from "@remix-run/react";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import Loading from "~/components/loading/loading";
+import { Input } from "~/components/ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
 import { toast } from "~/components/ui/use-toast";
 import MenuItemList from "~/domain/cardapio/components/menu-item-list/menu-item-list";
 import { menuItemPrismaEntity } from "~/domain/cardapio/menu-item.prisma.entity.server";
 import prismaClient from "~/lib/prisma/client.server";
 import { prismaIt } from "~/lib/prisma/prisma-it.server";
 import { badRequest, ok } from "~/utils/http-response.server";
+import { jsonParse } from "~/utils/json-helper";
 import tryit from "~/utils/try-it";
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -29,7 +32,8 @@ export async function loader({ request }: LoaderFunctionArgs) {
     // const sizeVariations = prismaClient.menuItemSize.findMany()
     const menuItemGroups = prismaClient.menuItemGroup.findMany({
         where: {
-            deletedAt: null
+            deletedAt: null,
+            visible: true
         }
     })
 
@@ -117,7 +121,7 @@ export async function action({ request }: LoaderFunctionArgs) {
             return badRequest(err)
         }
 
-        const returnedMessage = !item.active === true ? `O sabor "${item.name}" foi disativado` : `Nenhuma ação foi concluida`;
+        const returnedMessage = !item.active === true ? `O sabor "${item.name}" foi ativado` : `O sabor "${item.name}" foi desativado`;
 
         return ok(returnedMessage);
     }
@@ -136,20 +140,23 @@ export async function action({ request }: LoaderFunctionArgs) {
         }
 
         const [err, result] = await tryit(menuItemPrismaEntity.update(id, {
-            upcoming: !item.upcoming
+            upcoming: !item.upcoming,
+            visible: item.upcoming === true ? false : true
         }))
 
         if (err) {
             return badRequest(err)
         }
 
-        const returnedMessage = !item.upcoming === true ? `O sabor "\${item.name}" é um futuro lançamento` : `Nenhuma ação foi concluida`;
+        const returnedMessage = !item.upcoming === true ? `O sabor "\${item.name}" é um futuro lançamento` : `O sabor ${item.name} foi removido da futuro lançamento`;
 
         return ok(returnedMessage);
     }
 
     return null
 }
+
+type MenuItemVisibilityFilterOption = "active" | "lancamento-futuro" | "venda-pausada" | "inactive"
 
 export default function AdminGerenciamentoCardapioMainListLayout() {
 
@@ -186,17 +193,110 @@ export default function AdminGerenciamentoCardapioMainListLayout() {
                 {([listFlat, menuItemGroups, menuItemCategories]) => {
 
 
+                    const [items, setItems] = useState<any[]>(listFlat.filter(i => i.visible === true && i.active === true));
+                    const [search, setSearch] = useState("")
+
+                    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+                        const value = event.target.value
+                        setSearch(value)
+                        if (!value) return setItems(listFlat)
+                        const searchedItems = listFlat
+                            .filter(item => item.name?.toLowerCase().includes(value.toLowerCase()) || item.ingredients?.toLowerCase().includes(value.toLowerCase()))
+                        setItems(searchedItems)
+                    }
+
+                    const [currentFilter, setCurrentFilter] = useState<MenuItemVisibilityFilterOption | null>("active")
+
+                    const onChangeVisibilityFilter = (option: MenuItemVisibilityFilterOption) => {
+
+                        if (option === "active") {
+                            setCurrentFilter("active")
+                            setItems(items.filter(item => item.active === true))
+                        }
+
+                        if (option === "inactive") {
+                            setCurrentFilter("inactive")
+                            setItems(items.filter(item => item.active === false))
+                        }
+
+                        if (option === "lancamento-futuro") {
+                            setCurrentFilter("lancamento-futuro")
+                            setItems(items.filter(item => item.active === true && item.upcoming === true))
+                        }
+
+                        if (option === "venda-pausada") {
+                            setCurrentFilter("venda-pausada")
+                            setItems(items.filter(item => item.active === true && item.visible === false && item.upcoming === false))
+                        }
+                    }
+
+                    const [currentGroup, setCurrentGroup] = useState<MenuItemGroup["key"]>("pizza-salgadas")
+
+                    const onChangeMenuItemGroup = (groupKey: MenuItemGroup["key"]) => {
+                        setCurrentGroup(groupKey)
+                        setItems(listFlat.filter(i => i.MenuItemGroup?.key === groupKey))
+                    }
 
                     return (
+                        <div className="flex flex-col">
+                            <div className="flex flex-col gap-4 md:grid md:grid-cols-8 md:gap-x-4 md:items-center">
+                                <Select name="group"
+                                    onValueChange={(value) => {
+                                        const groupParsed = jsonParse(value)
+                                        setItems(listFlat.filter(i => {
+                                            if (i.MenuItemGroup === null) return true
+                                            return i.MenuItemGroup.key === groupParsed.key
+                                        }))
+                                    }}
+                                >
+                                    <SelectTrigger className="w-full md:col-span-2">
+                                        <SelectValue placeholder="Selecionar grupo..." />
+                                    </SelectTrigger>
+                                    <SelectContent id="group" >
+                                        <SelectGroup >
+                                            {menuItemGroups.map(g => {
+                                                return (
+                                                    <SelectItem key={g.id} value={JSON.stringify(g)} className="text-lg">{g.name}</SelectItem>
+                                                )
+                                            })}
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </Select>
 
-                        <MenuItemList
-                            // @ts-ignore
-                            initialItems={listFlat}
-                            // @ts-ignore
-                            groups={menuItemGroups}
-                            // @ts-ignore
-                            categories={menuItemCategories}
-                        />
+                                <Select
+                                    onValueChange={(value) => {
+                                        onChangeVisibilityFilter(value as MenuItemVisibilityFilterOption)
+                                    }}
+                                    defaultValue={"active"}
+                                >
+                                    <SelectTrigger className="w-full md:col-span-2">
+                                        <SelectValue placeholder="Filtrar vendas" />
+                                    </SelectTrigger>
+                                    <SelectContent >
+                                        <SelectItem value="active">Venda ativa</SelectItem>
+                                        <SelectItem value="lancamento-futuro">Lançamento futuro</SelectItem>
+                                        <SelectItem value="venda-pausada">Venda pausada</SelectItem>
+                                        <SelectItem value="inactive">Inativos</SelectItem>
+                                    </SelectContent>
+                                </Select>
+
+
+                                <div className="items-center col-span-5 md:col-span-4" >
+                                    <Input name="search" className="w-full" placeholder="Pesquisar..." onChange={(e) => handleSearch(e)} value={search} />
+                                </div>
+
+
+                            </div>
+
+                            <MenuItemList
+                                // @ts-ignore
+                                items={items}
+                                // @ts-ignore
+                                groups={menuItemGroups}
+                                // @ts-ignore
+                                categories={menuItemCategories}
+                            />
+                        </div>
                     )
 
                 }}
